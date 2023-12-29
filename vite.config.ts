72727f8068fc3089b0702f318ad135a6325e79d6
geoffreychen777 <geoffreychen777@gmail.com>
2023-12-29 17:51:45 +0000

    new version

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ rmSync("release", { recursive: true, force: true });
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    minify: false,
+    minify: true,
     reportCompressedSize: true,
     lib: {
       entry: path.resolve(__dirname, "src/main.ts"),
